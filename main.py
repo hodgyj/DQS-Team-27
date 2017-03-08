f0803067a551650e6d63ec235c7870b8dd3b7344
James Hodgson <hodgyj@outlook.com>
2017-03-08 14:40:05 +0000

    Made it pretty

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ class MainMenu(Frame):
         lblTitle.grid(row=0, column=0, columnspan=4, sticky=W+E, pady=10, padx=10)
 
     def createButtons(self):
-        btnAssign = Button(self, text='Assign', font=('MS', 16))
-        btnAssign.grid(row=1, column=1, columnspan=2, sticky=W+E)
-        btnReassign = Button(self, text='Reassign', font=('MS', 14))
+        btnAssign = Button(self, text='Assign', font=('MS', 16), background='#E91E63', foreground='#FFFFFF')
+        btnAssign.grid(row=1, column=1, columnspan=2, sticky=W+E, ipady=10, ipadx=30)
+        btnReassign = Button(self, text='Reassign', font=('MS', 12))
         btnReassign.grid(row=2, column=1, columnspan=2, sticky=W+E)
 
-        btnSearch = Button(self, text='Search', font=('MS', 12))
-        btnSearch.grid(row=3, column=0)
+        btnSearchTutors = Button(self, text='Search', font=('MS', 12))
+        btnSearchTutors.grid(row=3, column=0, pady=10)
         btnQuota = Button(self, text='Quotas', font=('MS', 12))
         btnQuota.grid(row=3, column=3)
 
