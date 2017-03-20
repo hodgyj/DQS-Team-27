2b12b6df76f96155dbab8ce41ac7e47ae1ed7f07
James Hodgson <hodgyj@outlook.com>
2017-03-20 14:52:01 +0000

    Buttons work (except quota)

--- mainmenu.py
+++ mainmenu.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from PIL import Image, ImageTk
 
+root = Tk()
+
 class MainMenu(Frame):
     # GUI setup
     def __init__(self, master):
@@ -9,6 +11,20 @@ class MainMenu(Frame):
         self.createTitle()
         self.createButtons()
 
+    def openAssign(self):
+        root.destroy()
+        import assign
+    
+    def openReassign(self):
+        root.destroy()
+        import reassign
+
+    def openSearch(self):
+        root.destroy()
+        import search
+
+    def openQuota(self):
+        print("no")
     
 
     def createTitle(self):
@@ -16,18 +32,17 @@ class MainMenu(Frame):
         lblTitle.grid(row=0, column=0, columnspan=4, sticky=W+E, pady=10, padx=20)
 
     def createButtons(self):
-        btnAssign = Button(self, text='Assign', font=('Segoe UI', 16), background='#2196F3', activebackground='#64B5F6', activeforeground='#FFFFFF', foreground='#FFFFFF')
+        btnAssign = Button(self, text='Assign', font=('Segoe UI', 16), background='#2196F3', activebackground='#64B5F6', activeforeground='#FFFFFF', foreground='#FFFFFF', command=self.openAssign)
         btnAssign.grid(row=1, column=1, columnspan=2, sticky=W+E, ipadx=40)
-        btnReassign = Button(self, text='Reassign', font=('Segoe UI', 12))
+        btnReassign = Button(self, text='Reassign', font=('Segoe UI', 12), command=self.openReassign)
         btnReassign.grid(row=2, column=1, columnspan=2, sticky=W+E)
 
-        btnSearch = Button(self, text='Search', font=('Segoe UI', 12), compound=LEFT)
+        btnSearch = Button(self, text='Search', font=('Segoe UI', 12), compound=LEFT, command=self.openSearch)
         btnSearch.grid(row=3, column=0, pady=10)
-        btnQuota = Button(self, text='Quotas', font=('Segoe UI', 12))
+        btnQuota = Button(self, text='Quotas', font=('Segoe UI', 12), command=self.openQuota)
         btnQuota.grid(row=3, column=3)
 
 def StartWindow():
-    root = Tk()
     root.title("Tutor & Tutee")
     root.resizable(0,0)
     app = MainMenu(root)
