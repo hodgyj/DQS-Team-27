a40254f2f907a59bd7cfa1022a87df10ba20ea67
James Hodgson <hodgyj@outlook.com>
2017-03-21 19:38:42 +0000

    Fixed iteration problem (see comment) The for loop was not resetting back to the start of the files when they finished. Apparently this is to do with how python handles opening files. Instead, I have stored the csv files as lists and iterate through them instead, modifying the lists instead of the actual csv reader objects. Then, once all of the tutors have been looped through, it writes the file.

--- assignCSVCode.py
+++ assignCSVCode.py
@@ -8,21 +8,41 @@ file1 = open('tutors.csv', 'r')
 file2 = open('tutees.csv', 'r')
 
 tutor = csv.reader(file1)
-next(tutor)
+# next(tutor)
 tutee = csv.reader(file2)
 
+# Store the csv files as lists
+tutorList = list(tutor)
+tuteeList = list(tutee)
 
 with open(tempFileName, 'w', newline='') as tempfile:
 	wrt = csv.writer(tempfile)
-	for row in tutor:
-		for row2 in tutee:
+	for row in tutorList:
+		for row2 in tuteeList:
 			degGroup = row[5]
 			tutorID = row[0]
 			degGroup2 = row2[5]
 			if degGroup == degGroup2:
 				row2[4] = tutorID
-				wrt.writerow(row2)
-			else:
-				wrt.writerow(row2)
+	
+	for row in tuteeList:
+		wrt.writerow(row)
 
 shutil.move(tempFileName, csvFileName)
+			
+# Left old code here for comparison
+
+# with open(tempFileName, 'w', newline='') as tempfile:
+# 	wrt = csv.writer(tempfile)
+# 	for row in tutor:
+# 		for row2 in tutee:
+# 			degGroup = row[5]
+# 			tutorID = row[0]
+# 			degGroup2 = row2[5]
+# 			if degGroup == degGroup2:
+# 				row2[4] = tutorID
+# 				wrt.writerow(row2)
+# 			else:
+# 				wrt.writerow(row2)
+
+
