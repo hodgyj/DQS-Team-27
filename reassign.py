bc3624cf70c37901086b1d536abded15738a828f
KT <KT>
2017-03-21 22:12:28 +0000

    fixes

--- reassign.py
+++ reassign.py
@@ -5,23 +5,23 @@ import shutil
 
 def reassignTutee(course, filename, tutfilename):
     with open(tutfilename) as csvfile:
-		assigned = False
-		assigned2 = False
+        assigned = False
+        assigned2 = False
         csvreader = csv.reader(csvfile)
         for row in csvreader:
             if (row[5] == course) and (numberOfStudents(row[0], filename) < int(row[4])):
                 tutor = row[0]
-				assigned = True
+                assigned = True
 
-		if (assigned == False):
-    		for row in csvreader:
-    			if (numberOfStudents(row[0], filename) < int(row[4])):
-                	tutor = row[0]
-					assigned2 = True
-		
-		if (assigned2 == False):
-    		message.showerror("No Tutor Avaliable", "There is no tutor currently avaliable to take this student.")
-		
+        if (assigned == False):
+            for row in csvreader:
+                if (numberOfStudents(row[0], filename) < int(row[4])):
+                    tutor = row[0]
+                    assigned = True
+        
+        if (assigned == False):
+            messagebox.showerror("No Tutor Avaliable", "There is no tutor currently avaliable to take this student.")
+        
     csvfile.close()
     return tutor
 
@@ -37,34 +37,34 @@ def numberOfStudents(tutor, filename):
 
 class Reassign(Frame):
 
-	def __init__(self, master):
-		Frame.__init__(self, master)
-		self.grid()
-		self.createTitle()
-		self.createIdEntry()
-		self.Submit()
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        self.grid()
+        self.createTitle()
+        self.createIdEntry()
+        self.Submit()
 
-	def createTitle(self):
+    def createTitle(self):
 
-		TitlePg = Label(self, text='Reassign Student', font=('Segoe UI light', 24), background="white")
-		TitlePg.grid(row=0, column=0, columnspan=4, sticky=W+E)
+        TitlePg = Label(self, text='Reassign Student', font=('Segoe UI light', 24), background="white")
+        TitlePg.grid(row=0, column=0, columnspan=4, sticky=W+E)
 
-	def createIdEntry(self):
-		
-		Label(self, text='Please enter ID of\nstudent to be reassigned:', 
-			font=('Segoe UI light', 18), background="white").grid(row=1, column=0, columnspan=4, sticky=W+E, padx=15)
-		self.entId=Entry(self, font=('Segoe UI', 18), fg='#2196F3', bg='white')
-		self.entId.grid(row=2, column=0, columnspan=4, sticky=W+E, padx=15)
+    def createIdEntry(self):
+        
+        Label(self, text='Please enter ID of\nstudent to be reassigned:', 
+            font=('Segoe UI light', 18), background="white").grid(row=1, column=0, columnspan=4, sticky=W+E, padx=15)
+        self.entId=Entry(self, font=('Segoe UI', 18), fg='#2196F3', bg='white')
+        self.entId.grid(row=2, column=0, columnspan=4, sticky=W+E, padx=15)
 
-	def Submit(self):
+    def Submit(self):
 
-	 	butSubmit = Button(self, text='OK',font=('Segoe UI light', 14), bg='#2196F3', activebackground='#64B5F6', fg='white', activeforeground='white', relief=FLAT, command=self.submitClicked)
-	 	butSubmit.grid(row=3, column=1, columnspan=1, sticky=W, pady=10, ipadx=2)
-	 	butCancel = Button(self, text='Cancel',font=('Segoe UI light', 14), bg='#2196F3', activebackground='#64B5F6', fg='white', activeforeground='white', relief=FLAT, command=self.openMain)
-	 	butCancel.grid(row=3, column=2, columnspan=1, sticky=E, pady=10, ipadx=2)
+         butSubmit = Button(self, text='OK',font=('Segoe UI light', 14), bg='#2196F3', activebackground='#64B5F6', fg='white', activeforeground='white', relief=FLAT, command=self.submitClicked)
+         butSubmit.grid(row=3, column=1, columnspan=1, sticky=W, pady=10, ipadx=2)
+         butCancel = Button(self, text='Cancel',font=('Segoe UI light', 14), bg='#2196F3', activebackground='#64B5F6', fg='white', activeforeground='white', relief=FLAT, command=self.openMain)
+         butCancel.grid(row=3, column=2, columnspan=1, sticky=E, pady=10, ipadx=2)
 
-	def submitClicked(self):
-    		identity = self.entId.get()
+    def submitClicked(self):
+        identity = self.entId.get()
         with open("tutees.csv") as csvfile:
             with open("temp.csv", "w", newline='') as tempfile:
                 csvreader = csv.reader(csvfile)
@@ -85,19 +85,19 @@ class Reassign(Frame):
         else:
             shutil.move("temp.csv", "tutees.csv")
             messagebox.showinfo("Reassigned Student", "Student Successfully Reassigned")
-		
-	def openMain(self):
-		import mainmenu
-		mainmenu.StartWindow()
-	
+        
+    def openMain(self):
+        import mainmenu
+        mainmenu.StartWindow()
+    
 
 def StartWindow():
-	root = Tk()
-	root.title("Reassign Student")
-	root.resizable(0,0)
-	app = Reassign(root)
-	app.configure(background="white")
-	root.mainloop()
+    root = Tk()
+    root.title("Reassign Student")
+    root.resizable(0,0)
+    app = Reassign(root)
+    app.configure(background="white")
+    root.mainloop()
 
 if __name__ == "__main__":
-	import login
+    import login
