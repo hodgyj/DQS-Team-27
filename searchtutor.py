adcba48df7fceb277b0f2d46054976ff5a528e58
Markuss Baumgarts <markuss.baumgarts@gmail.com>
2017-03-23 22:04:58 +0100

    Close button finally works

--- searchtutor.py
+++ searchtutor.py
@@ -1,8 +1,6 @@
 from tkinter import *
-from tkinter import messagebox,ttk
+from tkinter import messagebox, ttk
 import csv
-
-
 
 
 class Search(Frame):
@@ -11,13 +9,13 @@ class Search(Frame):
         Frame.__init__(self, master)
         self.grid()
         self.createTitle()
-        self.createInput()
+        self.createInput(master)
 
     def createTitle(self):
         lblTitle = Label(self, text='Tutor Search', font=('Segoe UI Light', 32), background="white")
         lblTitle.grid(row=0, column=0, columnspan=3, sticky=W + E, pady=5, padx=20)
 
-    def createInput(self):
+    def createInput(self,window):
         lblHelp = Label(self, text='Please enter the tutor ID:', font=('Segoe UI Light', 16), background="white")
         lblHelp.grid(row=1, column=0, columnspan=3, sticky=W + E)
 
@@ -29,9 +27,8 @@ class Search(Frame):
                            foreground='#FFFFFF', command=lambda: input_test(txtSearch.get()))
         btnSearch.grid(row=3, column=0, sticky=W + E, ipadx=10, pady=15, padx=10)
         btnCancel = Button(self, text='Cancel', font=('Segoe UI', 14), background='#2196F3', activebackground='#64B5F6',
-                           activeforeground='#FFFFFF', foreground='#FFFFFF')
+                           activeforeground='#FFFFFF', foreground='#FFFFFF', command = window.destroy)
         btnCancel.grid(row=3, column=2, sticky=W + E, ipadx=10, padx=10)
-
 
 
 def StartWindow():
@@ -42,14 +39,15 @@ def StartWindow():
     app.configure(background="white")
     root.mainloop()
 
+
 """Checks if given id is valid, and if it is - goes to the searchTut function and returns tutors id and name"""
+
 
 def input_test(userinput):
     tutorInfo = []
     with open('fileLoc.txt') as txtFile:
-           fileLocations = txtFile.readlines()
-           fileLocations = [line.strip() for line in fileLocations]
-
+        fileLocations = txtFile.readlines()
+        fileLocations = [line.strip() for line in fileLocations]
 
     with open(fileLocations[0]) as tutorFile:
 
@@ -69,34 +67,32 @@ def input_test(userinput):
                     else:
                         name = row[1] + ' ' + row[3] + " " + row[2]
                     tutorInfo.append(name)
-                    searchTut(userinput,tutorInfo,tuteeReader)
+                    searchTut(userinput, tutorInfo, tuteeReader)
 
                     break
             if not found:
                 messagebox.showerror("Input Error", "No such ID")
-       
 
 
 """Gets all the students in a list fot the given tutor"""
 
-def searchTut(tutid,info,tuteeFile):
+
+def searchTut(tutid, info, tuteeFile):
     studentList = []
     for row in tuteeFile:
-        childList =[]
+        childList = []
         if tutid == row[4]:
-                childList.append(row[0])
+            childList.append(row[0])
 
-                """Dealing with middle names"""
+            """Dealing with middle names"""
 
-                if row[3] == "":
-                    name = row[1] + ' ' + row[2]
-                else:
-                    name = row[1] + ' ' + row[3] + " " + row[2]
-                childList.append(name)
-                studentList.append(childList)
-    StartTree(studentList,info)
-
-
+            if row[3] == "":
+                name = row[1] + ' ' + row[2]
+            else:
+                name = row[1] + ' ' + row[3] + " " + row[2]
+            childList.append(name)
+            studentList.append(childList)
+    StartTree(studentList, info)
 
 
 """
@@ -104,17 +100,14 @@ Creates a new window, to output the table (should the code below be in a differe
 """
 
 
-
 class Treelist(Frame):
     # GUI setup
-    def __init__(self, master,sList,info):
+    def __init__(self, master, sList, info):
         Frame.__init__(self, master)
         self.pack()
-        self.list(sList,info)
+        self.list(sList, info)
 
-
-
-    def list(self,sList,info):
+    def list(self, sList, info):
         tree = ttk.Treeview(self)
         tree['show'] = 'headings'
         tree["columns"] = ("one", "two")
@@ -128,21 +121,18 @@ class Treelist(Frame):
 
         tree.pack()
 
-def StartTree(sList,info):
+
+def StartTree(sList, info):
     root = Tk()
     root.title("Tutees of " + info[1])
-    root.minsize(200,200)
+    root.minsize(200, 200)
 
-    app = Treelist(root,sList,info)
+    app = Treelist(root, sList, info)
     app.configure(background="white")
     root.mainloop()
 
 
-
-
-
 if __name__ == "__main__":
     import login
-
 
 
